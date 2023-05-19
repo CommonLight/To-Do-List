91b8b255e5721979fc31dc79000f57c6ffdf888c
Luke Erickson <mom2seth09@hotmail.com>
2023-05-19 18:20:06 -0500

    Added functionality to the task list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import List from './components/List';
+import List from './components/List'
+
+
 
 function App() {
   return (
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
 
 const List = () => {
-  return (
-    <div>List</div>
+    
+    const [taskList, setTaskList] = useState([])
+    const [completedTasksIndexes, setCompletedTasksIndexes] = useState([])
+
+    const addTask = () => {
+        const element = document.getElementById("task-input")
+        const value = element.value
+        const newTaskList = [...taskList, value]
+        setTaskList(newTaskList)
+        element.value = ""
+    }
+
+    const deleteTask = (currentIndex) => {
+        const newArray = [...taskList]
+        newArray.splice(currentIndex, 1)
+        setTaskList(newArray)
+    }
+
+    const taskToggle = (currentIndex) => {
+        const completedTasks = [...completedTasksIndexes];
+        const index = completedTasks.indexOf(currentIndex);
+        if (index >= 0) {
+          completedTasks.splice(index, 1);
+        } else {
+          completedTasks.push(currentIndex);
+        }  
+        setCompletedTasksIndexes(completedTasks);
+      };
+      
+    // const taskToggle = (currentIndex) => {
+    //     const isTaskCompleted = isCompleted(currentIndex)
+    //     if (isTaskCompleted) {
+    //     const newArray = [...completedTasksIndexes]
+    //     newArray.splice(currentIndex, 1)
+    //     setCompletedTasksIndexes(newArray)
+    //     } else {
+    //         const newCompletedTasks = [...completedTasksIndexes, currentIndex]
+    //         setCompletedTasksIndexes(newCompletedTasks)
+    //     }
+    // } 
+    
+    const isCompleted = (currentIndex) => {
+        return completedTasksIndexes.indexOf(currentIndex) >= 0
+    }
+
+    return (
+    <div className='wrapper'>
+        <div className="addTask">
+            <h1>Daily Task List</h1>
+            <input id="task-input" className="newTask" type="text" name="newTask" placeholder='Enter new task here...'/>
+            <br />
+            <button className="addButton" onClick={addTask}>ADD</button>
+        </div>
+        <div className="taskList">
+            <h2>TASK LIST</h2>
+            {taskList.map((task, index) => {
+                return (<div>
+                <p style={{textDecoration: isCompleted(index) ? "line-through" : null, display:"inline", fontSize: "18pt", color: 'black', marginRight: "20px"}}>{task}</p>
+                <input className= "checkBox" type="checkbox" onChange={() => taskToggle(index)} />
+                <button className="deleteButton" onClick={() => deleteTask(index)}>DELETE</button>
+            </div>)
+            })}
+        </div>
+
+    </div>
   )
 }
 
